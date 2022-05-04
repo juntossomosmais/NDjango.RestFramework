369805def9c2d93e251793d80957f0b301c84256
pablo <pablohenriquem@hotmail.com>
2022-05-04 11:19:30 -0300

    Testing pipe after rename project

--- Scripts/start-tests.sh
+++ Scripts/start-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -ex
-dotnet ef database update --project /app/WebApplication2/WebApplication2.csproj
+dotnet ef database update --project /app/AspNetCore.RestFramework.Sample/AspNetCore.RestFramework.Sample.csproj
 
-dotnet test TestProject1 --configuration Release --logger trx --logger "console;verbosity=normal" --settings "./runsettings.xml"
+dotnet test AspNetCore.RestFramework.Test --configuration Release --logger trx --logger "console;verbosity=normal" --settings "./runsettings.xml"
