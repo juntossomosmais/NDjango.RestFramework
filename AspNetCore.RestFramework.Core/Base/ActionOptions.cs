254fd77f09d658f6c2055c122678557a42e2b003
Guilherme Augusto Raduenz <guilherme.raduenz.br@avenuecode.com>
2022-08-09 16:00:59 -0300

    removed unused AllowList and AllowPost options from ActionOptions

--- AspNetCore.RestFramework.Core/Base/ActionOptions.cs
+++ AspNetCore.RestFramework.Core/Base/ActionOptions.cs
@@ -2,8 +2,6 @@ namespace AspNetCore.RestFramework.Core.Base;
 
 public class ActionOptions
 {
-    public bool AllowList { get; set; } = true;
-    public bool AllowPost { get; set; } = true;
     public bool AllowPatch { get; set; } = true;
     public bool AllowPut { get; set; } = true;
 }
